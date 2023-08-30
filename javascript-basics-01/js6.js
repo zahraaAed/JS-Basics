a21565771b4f219d08c2d7504ed8fdcae76dcb36
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-08-30 14:48:04 +0300

    js1-step6

---
+++ javascript-basics-01/js6.js
@@ -0,0 +1,4 @@
+var firstNumber = parseFloat(prompt("Enter the first number:"));
+var secondNumber = parseFloat(prompt("Enter the second number:"));
+var remainder=firstNumber%secondNumber;
+alert(remainder);
